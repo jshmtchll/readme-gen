2e6d96dd29d3214b7c68961cee41f238afb4b40b
josh mitchell <jshmtchll@gmail.com>
2021-04-10 11:37:26 -0500

    readme edit

--- Develop/index.js
+++ Develop/index.js
@@ -11,12 +11,28 @@ function promptUser() {
         {
             type: 'input',
             name: 'projectName',
-            message: 'What is the title of your project?'
+            message: 'What is the title of your project? (Required)',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title of your project.');
+                    return false;
+                }
+            }
         },
         {
             type: "input",
             name: 'description',
-            message: 'Enter a description of this project.'
+            message: 'Enter a description of this project. (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please eneter a short description of this project.');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -41,24 +57,39 @@ function promptUser() {
         {
             type: 'list',
             name: 'license',
-            message: 'Does this project have a license?',
+            message: 'Select a license for this project',
             choices: [
                 'MIT',
                 'Mozilla',
                 'Apache',
-                'CreativeCommons',
-                'None'
+                'CreativeCommons'
             ]
         },
         {
             type: 'input',
             name: 'githubUsername',
-            message: 'What is your Github username?'
+            message: 'What is your Github username? (Required)',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github username.');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'email',
-            message: 'What is your email?'
+            message: 'What is your email? (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email.');
+                    return false;
+                }
+            }
         },      
     ]);
     
--- README.md
+++ README.md
@@ -1,5 +1,30 @@
-# Professional README Generator Starter Code
+# README.md Generator: ES6+ and Node.js
 
-[How to create a Professional README](./readme-guide.md)
+## Description 
+  
+This is a command-line application that runs with Node.js that dynamically generates a README.md file based on input about your project.
 
+
+## Installation
+
+1. Run `npm install inquirer` in order to install the inquirer package to prompt user for input
+
+2. Make sure you are in the develop folder `cd develop`
+
+3. The application itself can be invoked by running `node index.js` in terminal
+
+## Usage 
+
+
+<a href="https://drive.google.com/file/d/1pY4OJohIyeNwOvW2_-cmKlDJBadi_UEy/view?usp=sharing" rel="generator demo">![demo](assets/images/democrop.gif)</a>
+
+When you run `node index.js`, the application uses the `inquirer` package to prompt you in the command line with a series of questions about your GitHub and about your project.
+
+The application then takes the user responses and writes a markdown file  using `fs.writeFile` including the responses the user put in the prompts 
+
+## Questions?
+
+GitHub: [@jshmtchll](https://api.github.com/users/jshmtchll)
+
+Email: jshmtchll@gmail.com
 
